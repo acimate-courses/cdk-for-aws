c27f9f95b4211b093dea912283fefeeb5cbdc72c
Sripada Mishra <sripada.mishra@gmail.com>
2024-04-11 23:31:44 +0530

    s3

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,44 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    const intelligentTieringConfigurations: s3.IntelligentTieringConfiguration={
+      name:'s3_tier',
+      archiveAccessTierTime: cdk.Duration.days(90),
+      deepArchiveAccessTierTime:cdk.Duration.days(180)
+   }
+
+   const access_bucket = new s3.Bucket(this,'InfraAccessBucket',{
+    bucketName :'acimate-test-access-bucket-999',
+    blockPublicAccess : s3.BlockPublicAccess.BLOCK_ALL,
+    encryption:s3.BucketEncryption.S3_MANAGED,
+    enforceSSL:true,
+    versioned: true,
+    intelligentTieringConfigurations : [intelligentTieringConfigurations],
+    objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+    publicReadAccess:false
+   }); 
+
+   const bucket = new s3.Bucket(this,'InfraBucket',{
+    bucketName :'acimate-test-bucket-999',
+    blockPublicAccess : s3.BlockPublicAccess.BLOCK_ALL,
+    encryption:s3.BucketEncryption.S3_MANAGED,
+    enforceSSL:true,
+    versioned: true,
+    intelligentTieringConfigurations : [intelligentTieringConfigurations],
+    objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+    publicReadAccess:false,
+    serverAccessLogsBucket:access_bucket
+   });
+  }
+}
